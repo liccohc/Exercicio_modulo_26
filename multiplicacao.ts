c137d59e19dcb3170cdd55d2d4b6498f428af4df
Pedro Xavier <155593419+liccohc@users.noreply.github.com>
2024-08-02 18:13:19 -0300

    Add files via upload

---
+++ multiplicacao.ts
@@ -0,0 +1,7 @@
+//funcao para calcular uma area
+function calcularArea (base: number, altura: number): number {
+    const area = base * altura;
+    return area;
+};
+const resultado = calcularArea(168, 2.37);
+console.log(resultado);
---
+++ saudacao.js
@@ -0,0 +1,9 @@
+"use strict";
+//funcão para retornar Olá+nome
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+;
+const seuNome = "Pedro";
+const saudacaoSeuNome = saudacao(seuNome);
+console.log(saudacaoSeuNome);
